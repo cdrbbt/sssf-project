e1d5abb7131683afefba83c6c686472b911244f6
cdrbbt <egor.bykadorov@gmail.com>
2019-04-22 23:07:41 +0300

    hand detection

--- models/Cards.js
+++ models/Cards.js
@@ -18,15 +18,93 @@ const sortHand = (hand) => {
   });
 };
 
-const checkRoyalFlush = (hand) => {
-  if (hand[0].value !== 14) return { found: false, value: null };
-
-  return true
+const checkStraighFlush = (hand) => {
+  for (let i = 1; i < 5; i++) {
+    const filtered = hand.filter(val => val.suit === i);
+    if (filtered.length > 4) {
+      const straight = checkStraight(filtered);
+      if (straight.found) return straight;
+    }
+  }
+  return { found: false, value: null };
 };
 
-const checkPairs = (hand) => {
+const checkFourOfKind = (hand) => {
+  for (let i = 3; i < hand.length; i++) {
+    if (hand[i].value === hand[i - 1].value && hand[i].value === hand[i - 2].value
+      && hand[i].value === hand[i - 3].value) {
+      return { found: true, value: hand[i].value };
+    }
+  }
+  return { found: false, value: null };
+};
+
+const checkFullHouse = (hand) => {
+  const triple =checkThreeOfKind(hand);
+  if (triple.found) {
+    const pair = checkPair(hand.filter(val => val.value !== triple.value));
+    if (pair.found) return { found: true, value: triple.value };
+  }
+  return { found: false, value: null };
+};
+
+const checkFlush = (hand) => {
+  for (let i = 1; i < 5; i++) {
+    const filtered = hand.filter(val => val.suit === i);
+    if (filtered.length > 5) {
+      return { found: true, value: filtered[0].value };
+    }
+  }
+  return { found: false, value: null };
+};
+
+const checkStraight = (hand) => {
+  let start = hand[0];
+  let previous = hand[0];
+  let straightCounter = 0;
+  for (let i = 1; i< hand.length; i++) {
+    if (hand[i].value === previous.value) continue;
+    if (hand[i].value === (previous.value - 1)) {
+      previous = hand[i];
+      straightCounter++;
+      if (straightCounter === 4) return { found: true, value: start.value };
+    }
+    start = hand[i];
+    straightCounter = 0;
+  }
+  return { found: false, value: null }
+};
+
+const checkThreeOfKind = (hand) => {
+  for (let i = 2; i < hand.length; i++) {
+    if (hand[i].value === hand[i - 1].value && hand[i].value === hand[i - 2].value) {
+      return { found: true, value: hand[i].value };
+    }
+  }
+  return { found: false, value: null };
+};
+
+const checkTwoPair = (hand) => {
+  let firstPair = true;
+  let pairValue = null;
+  for (let i = 1; i < hand.length; i++) {
+    if (hand[i].value === hand[i - 1].value) {
+      if (firstPair) {
+        firstPair = false;
+        pairValue = hand[i];
+      } else {
+        return { found: true, value: pairValue.value };
+      }
+    }
+  }
+  return { found: false, value: null };
+};
+
+const checkPair = (hand) => {
   for (let i = 1; i < hand.length; i++) {
     if (hand[i].value === hand[i - 1].value) return { found: true, value: hand[i].value };
   }
   return { found: false, value: null };
 };
+
+const checkHighCard = hand => ({ found: true, value: hand[0].value });
